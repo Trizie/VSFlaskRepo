66c50694f029aad72df064dc0b68fb9a690c7898
Trizie <patrizia.stutzenstein@gmx.at>
2024-07-23 09:10:50 +0200

    update views.py

--- website/views.py
+++ website/views.py
@@ -12,39 +12,18 @@ views = Blueprint('views', __name__)
 def home():
 
     try:
-        #lbm = Lebensmittel(Barcode=1238374, LebensmittelName="Olivenöl", Anzahl=2)
-        #db.session.add(lbm)
-        #db.session.commit()
+        # lbm = Lebensmittel(Barcode=1238374, LebensmittelName="Olivenöl", Anzahl=2)
+        # db.session.add(lbm)
+        # db.session.commit()
 
-        #lbm = Lebensmittel.query.all()[0]
-        #print(lbm)
-        list=["brot"]
-        anzahl=[1]
+        # lbm = Lebensmittel.query.all()[0]
+        # print(lbm)
+        
+        list = []
+        anzahl=[]
         anzahl = db.session.execute(db.select(Lebensmittel.Anzahl)).scalars().all()
         list = db.session.execute(db.select(Lebensmittel.LebensmittelName)).scalars().all()
 
-        #print('Creating Table Started =====')
-        #cur = mysql.cursor()
-        #cur.execute(
-        #    '''
-        #    SELECT Lebensmittel FROM lebensmittel
-        #    '''
-        #)
-        #list = ["Brot"]
-        #item = cur.fetchall()
-        #list = [row[0] for row in item]
-
-        #cur.execute(
-        #    '''
-        #    SELECT Anzahl FROM lebensmittel
-        #    '''
-        #)
-        #anzahl = [1]
-        #item = cur.fetchall()
-        #anzahl = [row[0] for row in item]
-
-        #cur.close()
-        #print('Items Table Created =====')
     except Exception as e:
         print("Error while creating table",e)
 
